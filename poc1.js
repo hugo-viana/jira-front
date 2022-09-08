9fe3a4b21f3f943b6e54f3623d68f94fa94cf61f
hugo-viana <79479606+hugo-viana@users.noreply.github.com>
2022-09-08 14:35:20 +0200

    Create poc1.js

---
+++ poc1.js
@@ -0,0 +1,16 @@
+const person = {
+    name: 'Wes',
+    job: 'Web Developer',
+    city: 'Hamilton',
+    bio: 'Wes is a really cool guy that loves to teach web development!'
+}
+// And then create our markup:
+const markup = `
+ <div class="person">
+    <h2>
+        ${person.name}
+    </h2>
+    <p class="location">${person.city}</p>
+    <p class="bio">${person.bio}</p>
+ </div>
+`;
